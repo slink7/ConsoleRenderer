e88484b55c058269156c1f755a0a7f36b4ee25a7
slink <slink794@gmail.com>
2023-10-22 23:19:42 +0200

    Added linked lists

---
+++ inc/t_linked_list.h
@@ -0,0 +1,21 @@
+#ifndef T_LINKED_LIST
+#define T_LINKED_LIST
+
+typedef struct s_link t_link;
+
+typedef struct s_link
+{
+	t_link *next;
+	void *data;
+}	t_link;
+
+t_link	*create_link(void *data);
+void	free_link(t_link **link);
+void	print_link(t_link *link, char *format);
+void	push_back_link(t_link **link, void *data);
+void	push_front_link(t_link **link, void *data);
+void	*get_at_link(t_link *link, int i);
+void		**to_array_link(t_link *link);
+int		get_length_link(t_link *link);
+
+#endif
---
+++ src/t_linked_list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "t_linked_list.h"
+
+t_link	*create_link(void *data)
+{
+	t_link *out = malloc(sizeof(t_link));
+	out->data = data;
+	out->next = 0;
+	return out;
+}
+
+void	free_link(t_link **link)
+{
+	if (!(*link))
+		return ;
+	free_link(&(*link)->next);
+	free(*link);
+}
+
+void	print_link(t_link *link, char *format)
+{
+	if (!link) return;
+	printf("[%ld]", (long) link->data);
+	if (link->next) {
+		printf("->");
+		print_link(link->next, format);
+	}
+	else
+		printf("\n");
+}
+
+void	push_back_link(t_link **link, void *data)
+{
+	if (!(*link)) {
+		*link = create_link(data);
+		return;
+	}
+	push_back_link(&(*link)->next, data);
+}
+
+void	push_front_link(t_link **link, void *data)
+{
+	if (!(*link)) {
+		*link = create_link(data);
+		return;
+	}
+	t_link *temp = create_link(data);
+	temp->next = *link;
+	*link = temp;
+}
+
+void	*get_at_link(t_link *link, int i)
+{
+	if (i < 1)
+		return link->data;
+	return get_at_link(link->next, i - 1);
+}
+
+void	**to_array_link(t_link *link)
+{
+	int len = get_length_link(link);
+	void **out = malloc(sizeof(void *) * len);
+	for (int k = 0; link; link = link->next) {
+		out[k] = link->data;
+		k++;
+	}
+	return out;
+}
+
+int		get_length_link(t_link *link)
+{
+	return (link->next) ? get_length_link(link->next) + 1 : 1;
+}
